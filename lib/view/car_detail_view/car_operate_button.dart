80cecd6555046294c761bc4323f05efc8440bd1f
cos-overclock <60503661+cos-overclock@users.noreply.github.com>
2023-03-22 09:18:10 +0900

    add message send state

--- lib/view/car_detail_view/car_operate_button.dart
+++ lib/view/car_detail_view/car_operate_button.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 
-class CarOperateButton extends StatelessWidget {
+import 'package:deepsky_flutter/provider/send_state_provider.dart';
+
+class CarOperateButton extends ConsumerWidget {
   const CarOperateButton(
       {super.key, required this.text, required this.name, required this.path});
 
@@ -11,7 +14,9 @@ class CarOperateButton extends StatelessWidget {
   final String path;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final sendState = ref.watch(sendStateProvider);
+
     return Padding(
       padding: const EdgeInsets.only(left: 5),
       child: OutlinedButton(
@@ -20,25 +25,55 @@ class CarOperateButton extends StatelessWidget {
             borderRadius: BorderRadius.circular(10), //角の丸み
           ),
         ),
-        onPressed: () => {},
+        onPressed: sendState != null
+            ? null
+            : () async {
+                ref.watch(sendStateProvider.notifier).state = text;
+                await Future.delayed(const Duration(seconds: 2));
+                ref.watch(sendStateProvider.notifier).state = null;
+                if (!context.mounted) return;
+                text != 'アンロック'
+                    ? ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text(
+                          'メッセージを送信しました',
+                          style: TextStyle(
+                              color: Theme.of(context).colorScheme.onPrimary),
+                        ),
+                        backgroundColor: Theme.of(context).colorScheme.primary,
+                        duration: const Duration(seconds: 1),
+                      ))
+                    : ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text(
+                          'メッセージの送信に失敗しました',
+                          style: TextStyle(
+                              color: Theme.of(context).colorScheme.onError),
+                        ),
+                        backgroundColor: Theme.of(context).colorScheme.error,
+                        duration: const Duration(seconds: 1),
+                      ));
+              },
         child: SizedBox(
           height: 80,
           width: 80,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SvgPicture.asset(
-                path,
-                semanticsLabel: 'State Icon',
-                width: 30,
-                colorFilter: ColorFilter.mode(
-                  Theme.of(context).colorScheme.primary,
-                  BlendMode.srcIn,
+          child: sendState == text
+              ? const Center(child: CircularProgressIndicator())
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset(
+                      path,
+                      semanticsLabel: 'State Icon',
+                      width: 30,
+                      colorFilter: ColorFilter.mode(
+                        sendState == null
+                            ? Theme.of(context).colorScheme.primary
+                            : Theme.of(context).colorScheme.outline,
+                        BlendMode.srcIn,
+                      ),
+                    ),
+                    AutoSizeText(text, maxLines: 1),
+                  ],
                 ),
-              ),
-              AutoSizeText(text, maxLines: 1),
-            ],
-          ),
         ),
       ),
     );
